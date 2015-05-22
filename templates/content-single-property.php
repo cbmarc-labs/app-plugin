f9f1d27a1b6a3a8adc05ad1a84031a51e35381d4
cbmarc-labs <cbmarc@gmail.com>
2015-05-22 18:52:39 +0200

    minor changes

--- templates/content-single-property.php
+++ templates/content-single-property.php
@@ -1,5 +1,11 @@
-SINGLE PROPERTY
+<div class="col-xs-12">
+
+	<a href="<?php the_permalink(); ?>">
+		<h3 class="page-header"><?php the_title(); ?></h3>
+	</a>
 
 <?php 
 app_get_template( 'single-property/property-image.php' );
 ?>
+
+</div>

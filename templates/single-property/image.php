cfe9e17d40269dbfd409902b58ce21843ce04f90
cbmarc-labs <cbmarc@gmail.com>
2015-07-06 18:25:03 +0200

    minor changes

--- templates/single-property/image.php
+++ templates/single-property/image.php
@@ -13,7 +13,6 @@ global $post, $property;
 ?>
 
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-	<div class="carousel-inner" role="listbox">
 
 <?php
 
@@ -21,19 +20,27 @@ $images_ids = $property->get_gallery_attachment_ids();
 
 if( $images_ids ) {
 	$images_array = explode( ",", $images_ids );
+	$it = 0;
 	$active = 'active';
 	
+	$indicators = '';
+	$inner = '';
 	foreach( $images_array as $id ) {
-		echo '<div class="item ' . $active . '">';
-		echo '<img width="100%;" style="height:400px;" src="' . wp_get_attachment_url( $id ) . '">';
-		echo '</div>';
+		$indicators .= '<li data-target="#carousel-example-generic" data-slide-to="' . $it . '" class="' . $active . '"></li>'; 
+		$inner .= '<div class="item ' . $active . '"><img width="100%;" style="height:400px;" src="' . wp_get_attachment_url( $id ) . '"></div>';
 		
 		$active = '';
+		$it ++;
 	}
 }
 
 ?>
 
+<ol class="carousel-indicators">
+<?php echo $indicators; ?>
+</ol>
+<div class="carousel-inner" role="listbox">
+<?php echo $inner; ?>
 </div>
 <!-- Controls -->
   <a class="left carousel-control" onclick="jQuery(this).closest('.carousel').carousel('prev');" role="button" data-slide="prev">

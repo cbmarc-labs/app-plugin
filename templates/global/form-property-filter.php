8b91fba0ca19967f01f52103a81f94264c1dd8a5
cbmarc-labs <cbmarc@gmail.com>
2015-07-06 11:54:12 +0200

    minor changes

--- templates/global/form-property-filter.php
+++ templates/global/form-property-filter.php
@@ -42,7 +42,7 @@ if( isset( $wp_query->query_vars['min_m2'] ) && !empty( $wp_query->query_vars['m
     			<!-- Brand and toggle get grouped for better mobile display -->
     			<div class="navbar-header">
     				<div class="col-xs-12">
-   						<button style="width:100%" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+   						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
        						<span class="sr-only">Toggle search</span>
        						<span class="glyphicon glyphicon-search">&nbsp;<?php _e( 'Search', 'app' ); ?></span>
 						</button>

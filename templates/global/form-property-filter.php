3ea479e013384f515586e6f5a78c64cf35fff276
cbmarc-labs <cbmarc@gmail.com>
2015-07-09 12:57:29 +0200

    minor changes

--- templates/global/form-property-filter.php
+++ templates/global/form-property-filter.php
@@ -182,7 +182,7 @@ if (isset($wp_query->query_vars['feature'] ) && !empty( $wp_query->query_vars['f
 					<span><label style="display:inherit;">&nbsp;/&nbsp;</label></span>
 					<span><label id="nouislider-value-upper" class="currency" style="display:inherit;"></label></span>
 				</div>
-				<div id="nonlinear" style="margin:12px 12px 0 12px;"></div>
+				<div id="nonlinear" style="margin:12px;"></div>
 				<input type="hidden" name="min_price" value="0">
 				<input type="hidden" name="max_price" value="0">
 			</div>

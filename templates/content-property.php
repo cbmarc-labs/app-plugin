f4843527b650db0d5fdd6aa238d639c354256630
marc <cbmarc@gmail.com>
2015-07-12 14:24:19 +0200

    minor changes

--- templates/content-property.php
+++ templates/content-property.php
@@ -13,8 +13,20 @@ global $property;
 
 <div class="col-sm-6 col-md-4">
 	<div class="box-effect1">
-		<div class="thumbnail">
+		<div class="thumbnail featured">
 		
+			<?php 
+			
+			$featured = get_post_meta( $post->ID, '_property_featured', 1 );
+			
+			if ( $featured ) :
+			?>
+			<div class="triangle-top-left">
+				<p class="featured-text"><?php _e( 'Featured', 'app' ); ?></p>
+			</div>
+			
+			<?php endif; ?>
+			
 			<a href="<?php echo esc_url( get_permalink() ); ?>" style="display: block !important;">
 				<?php echo $property->get_post_thumbnail(); ?>
 			</a>

c5b4fa464967ca6f271e76612c8123cef5d9409e
marc <cbmarc@gmail.com>
2015-07-13 22:05:08 +0200

    minor changes

--- templates/single-property/form-contact.php
+++ templates/single-property/form-contact.php
@@ -28,12 +28,12 @@ if ( isset( $wp_query->query_vars['status'] )
 	
 		<div class="form-group col-xs-12" style="padding:0;margin:0;">
 			<label for="name"><?php _e( 'Name', 'app' ); ?> *</label>
-			<input style="width:100%;" id="name" type="text" minlength="2" maxlength="50" name="name" value="marc" required />
+			<input style="width:100%;" id="name" type="text" minlength="2" maxlength="50" name="name" value="" required />
 		</div>
 		
 		<div class="form-group col-xs-12" style="padding:0;margin:0;">
 			<label for="email" class="control-label"><?php _e( 'Email', 'app' ); ?> *</label>
-			<input id="email" type="email" name="email" minlength="2" maxlength="50" class="form-control" value="cbmarc@gmail.com" required />
+			<input id="email" type="email" name="email" minlength="2" maxlength="50" class="form-control" value="" required />
 		</div>
 		
 		<div class="form-group col-xs-12" style="padding:0;margin:0;">

94483d33d5743ccb5126769d50378f40c577cf36
cbmarc-labs <cbmarc@gmail.com>
2015-07-15 15:51:29 +0200

    minor changes

--- includes/class-app-form-handler.php
+++ includes/class-app-form-handler.php
@@ -43,20 +43,34 @@ class APP_Form_Handler
 			return;
 		}
 		
+		$title		= $_POST[ 'title' ];
+		$permalink	= $_POST[ 'permalink' ];
+		
+		$name		= ! empty( $_POST[ 'name' ] ) ? sanitize_text_field( $_POST[ 'name' ] ) : '';
+		$email		= ! empty( $_POST[ 'email' ] ) ? sanitize_email( $_POST[ 'email' ] ) : '';
+		$phone		= ! empty( $_POST[ 'phone' ] ) ? sanitize_text_field( $_POST[ 'phone' ] ) : '';
+		$message	= ! empty( $_POST[ 'message' ] ) ? sanitize_text_field( $_POST[ 'message' ] ) : '';
+		
+		$message .= '<br><a href="' . $_POST[ 'permalink' ] . '">' . $_POST[ 'permalink' ] . '</a>';
+		
+		// TODO check and display form errors
+		if ( empty( $name ) ) {}		
+		if ( ! is_email( $email ) ) {}
+		
 		$data = array(
-				'post_title'   => $_POST[ 'title' ],
-				'post_content' => $_POST[ 'message' ] . '<br><a href="' . $_POST[ 'permalink' ] . '">' . $_POST[ 'permalink' ] . '</a>',
+				'post_title'   => $title,
+				'post_content' => $message,
 				'post_status'  => 'publish',
 				'post_type'    => 'message'
 		);
 		
 		$post_id = wp_insert_post( $data );
 		
-		update_post_meta( $post_id, '_message_name', $_POST['name'] );
-		update_post_meta( $post_id, '_message_email', $_POST['email'] );
-		update_post_meta( $post_id, '_message_phone', $_POST['phone'] );
+		update_post_meta( $post_id, '_message_name', $name );
+		update_post_meta( $post_id, '_message_email', $email );
+		update_post_meta( $post_id, '_message_phone', $phone );
 		
-		$received_url = add_query_arg( 'status', 'sent', $_POST[ 'permalink' ] );
+		$received_url = add_query_arg( 'status', 'sent', $permalink );
 		
 		wp_safe_redirect( $received_url );
 		

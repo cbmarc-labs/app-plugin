9d42b4287d5253db97dda187b52424e3a59e4a61
marc <marc@192.168.1.34>
2015-09-21 12:53:17 +0200

    minor changes

--- includes/admin/class-app-admin.php
+++ includes/admin/class-app-admin.php
@@ -35,6 +35,7 @@ class APP_Admin
 		// Classes
 		include_once( 'class-app-admin-post-types.php' );		
 		include_once( 'class-app-admin-meta-boxes.php' );
+		include_once( 'class-app-admin-settings.php' );
 		
 		if ( ! is_ajax() ) {
 			include_once( 'class-app-admin-menus.php' );
